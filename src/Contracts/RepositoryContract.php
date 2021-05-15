f13c44d3caa8f4d5563fe3a551d55a354a294ae6
Kartav1K <roman.varkuta@gmail.com>
2021-05-15 22:22:56 +0300

    Apply union typehint contracts fro repo

--- src/Contracts/RepositoryContract.php
+++ src/Contracts/RepositoryContract.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace Kartavik\WhiteBIT\Api\Contracts;
 
+use Doctrine\Common\Collections\Collection;
 use Kartavik\WhiteBIT\Api\Contracts\Data\PairContract;
 use Kartavik\WhiteBIT\Api\Contracts\Data\V1\MarketActivityContract;
 use Kartavik\WhiteBIT\Api\Interval;
 
 interface RepositoryContract
 {
-    public function getMarketsInfoV1(): iterable;
+    public function getMarketsInfoV1(): Collection|array;
 
-    public function getMarketsActivityV1(): iterable;
+    public function getMarketsActivityV1(): Collection|array;
 
-    public function getSingleMarketActivityV1(PairContract $pair): mixed;
+    public function getSingleMarketActivityV1(PairContract $pair): MarketActivityContract|array;
 
     public function getKline(
         PairContract $pair,
@@ -24,7 +25,7 @@ interface RepositoryContract
         int $limit = 1440
     ): iterable;
 
-    public function getSymbolsV1(): iterable;
+    public function getSymbolsV1(): Collection|array;
 
-    public function getTradeHistoryV1(PairContract $pair, int $lastId = 1, int $limit = 50): iterable;
+    public function getTradeHistoryV1(PairContract $pair, int $lastId = 1, int $limit = 50): Collection|array;
 }
--- src/Repository.php
+++ src/Repository.php
@@ -8,8 +8,13 @@ use Carbon\Carbon;
 use Doctrine\Common\Collections\Collection;
 use Http\Promise\Promise;
 use Kartavik\WhiteBIT\Api\Contracts\Data\PairContract;
+use Kartavik\WhiteBIT\Api\Contracts\Data\V1\MarketActivityContract;
+use Kartavik\WhiteBIT\Api\Contracts\Data\V1\MarketInfoContract;
 use Kartavik\WhiteBIT\Api\Data\V1\MarketActivity;
 
+/**
+ * @psalm-import-type MarketInfoV1Data from Action
+ */
 class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAsyncAwareContract
 {
     public function __construct(
@@ -18,7 +23,8 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
     ) {
     }
 
-    public function getMarketsInfo(Version $version): iterable
+    /** @return MarketInfoV1Data|Collection<int, MarketInfoContract> */
+    public function getMarketsInfo(Version $version): Collection|array
     {
         return match (true) {
             $version->equals(Version::V_1()) => $this->getMarketsInfoV1(),
@@ -26,7 +32,8 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
         };
     }
 
-    public function getMarketsInfoV1(): iterable
+    /** @return MarketInfoV1Data|Collection<int, MarketInfoContract> */
+    public function getMarketsInfoV1(): Collection|array
     {
         return $this->getMarketsInfoV1Async()->wait();
     }
@@ -34,13 +41,13 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
     public function getMarketsInfoV1Async(): Promise
     {
         return $this->http->get(Version::V_1(), Contracts\HttpContract::PUBLIC_API, Action::MARKETS)
-            ->then(fn (array $result): iterable => $this->parse(
+            ->then(fn (array $result): Collection|array => $this->parse(
                 $result,
                 fn (array $data): Collection => $this->parser->parseMarketInfoV1Collection($data)
             ));
     }
 
-    public function getMarketsActivityV1(): iterable
+    public function getMarketsActivityV1(): Collection|array
     {
         return $this->getMarketsActivityV1Async()->wait();
     }
@@ -48,13 +55,13 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
     public function getMarketsActivityV1Async(): Promise
     {
         return $this->http->get(Version::V_1(), Contracts\HttpContract::PUBLIC_API, Action::TICKERS)
-            ->then(fn (array $result): iterable => $this->parse(
+            ->then(fn (array $result): Collection|array => $this->parse(
                 $result,
                 fn (array $data): Collection => $this->parser->parseMarketActivityV1Collection($data)
             ));
     }
 
-    public function getSingleMarketActivityV1(PairContract $pair): mixed
+    public function getSingleMarketActivityV1(PairContract $pair): MarketActivityContract|array
     {
         return $this->getSingleMarketActivityV1Async($pair)->wait();
     }
@@ -63,7 +70,7 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
     {
         return $this->http->get(Version::V_1(), Contracts\HttpContract::PUBLIC_API, Action::TICKER, [
             'market' => $pair->getMarketName(),
-        ])->then(fn (array $result): mixed => $this->parse(
+        ])->then(fn (array $result): MarketActivityContract|array => $this->parse(
             $result,
             fn (array $data): MarketActivity => $this->parser->parseSingleMarketActivity($data, $pair)
         ));
@@ -92,13 +99,13 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
             'end' => $end ?? Carbon::now()->getTimestamp(),
             'interval' => $interval,
             'limit' => $limit,
-        ])->then(fn (array $result): iterable => $this->parse(
+        ])->then(fn (array $result): Collection|array => $this->parse(
             $result,
             fn (array $data): Collection => $this->parser->parseKlineCollection($data, $pair)
         ));
     }
 
-    public function getSymbolsV1(): iterable
+    public function getSymbolsV1(): Collection|array
     {
         return $this->getSymbolsV1Async()->wait();
     }
@@ -106,13 +113,13 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
     public function getSymbolsV1Async(): Promise
     {
         return $this->http->get(Version::V_1(), Contracts\HttpContract::PUBLIC_API, Action::SYMBOLS)
-            ->then(fn (array $result): iterable => $this->parse(
+            ->then(fn (array $result): Collection|array => $this->parse(
                 $result,
                 fn (array $data): Collection => $this->parser->parseMarketCollection($data)
             ));
     }
 
-    public function getTradeHistoryV1(PairContract $pair, int $lastId = 1, int $limit = 50): iterable
+    public function getTradeHistoryV1(PairContract $pair, int $lastId = 1, int $limit = 50): Collection|array
     {
         return $this->getTradeHistoryV1Async($pair, $lastId, $limit)->wait();
     }
@@ -123,7 +130,7 @@ class Repository implements Contracts\RepositoryContract, Contracts\RepositoryAs
             'market' => $pair->getMarketName(),
             'lastId' => $lastId,
             'limit' => $limit,
-        ])->then(fn (array $result): iterable => $this->parse(
+        ])->then(fn (array $result): Collection|array => $this->parse(
             $result,
             fn (array $data): Collection => $this->parser->parseTradeHistoryCollection($pair, $lastId, $data)
         ));
